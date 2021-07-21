26eb80d6b789ec30875e37f6647ff054650fb916
Daniel Yates <me@meorawr.io>
2021-07-21 01:12:44 +0100

    Add initial tests for taint propagation to values
    These verify that the various push* functions in the C API will
    apply the taint from the state to the newly created values, or that
    they NULL-initialize the field if no taint is present. This also
    covers the lua_getvaluetaint and lua_getobjecttaint functions for
    each type within the API.

--- test/security.c
+++ test/security.c
@@ -6,6 +6,53 @@
 
 static lua_TaintInfo test_taint = { "Test Taint", NULL };
 
+static int push_number(lua_State *L) { lua_pushnumber(L, 123.456); return 0; }
+static int push_integer(lua_State *L) { lua_pushinteger(L, 100); return 0; }
+static int push_nil(lua_State *L) { lua_pushnil(L); return 0; }
+static int push_string(lua_State *L) { lua_pushliteral(L, "string value"); return 0; }
+static int push_boolean(lua_State *L) { lua_pushboolean(L, 1); return 0; }
+static int push_table(lua_State *L) { lua_createtable(L, 0, 0); return 0; }
+static int push_lightuserdata(lua_State *L) { lua_pushlightuserdata(L, &test_taint); return 0; }
+static int push_userdata(lua_State *L) { lua_newuserdata(L, 0); return 0; }
+static int push_cclosure(lua_State *L) { lua_pushcclosure(L, &push_cclosure, 0); return 0; }
+static int push_thread(lua_State *L) { lua_newthread(L); return 0; }
+
+/****************************************************************************
+ * Value Test Vectors
+ ****************************************************************************/
+
+struct LuaValueVector {
+  const char *name;
+  lua_CFunction func;
+};
+
+struct LuaValueVector lua_value_vectors[] = {
+  { "number", push_number },
+  { "integer", push_integer },
+  { "nil", push_nil },
+  { "string", push_string },
+  { "boolean", push_boolean },
+  { "table", push_table },
+  { "lightuserdata", push_lightuserdata },
+  { "userdata", push_userdata },
+  { "function", push_cclosure },
+  { "thread", push_thread },
+  { NULL, NULL }
+};
+
+struct LuaValueVector lua_object_vectors[] = {
+  { "string", push_string },
+  { "table", push_table },
+  { "userdata", push_userdata },
+  { "function", push_cclosure },
+  { "thread", push_thread },
+  { NULL, NULL }
+};
+
+/****************************************************************************
+ * Basic State Taint Tests
+ ****************************************************************************/
+
 void test_startup_taint(void) {
   lua_State *L = luaL_newstate();
   TEST_CHECK(lua_gettaint(L) == NULL);
@@ -36,10 +83,68 @@ void test_forced_taint_override(void) {
   lua_close(L);
 }
 
+/****************************************************************************
+ * State => Value and Object Taint Propagation Tests
+ ****************************************************************************/
+
+void test_push_values_with_taint(void) {
+  for (struct LuaValueVector *vec = lua_value_vectors; vec->name; ++vec) {
+    TEST_CASE(vec->name);
+    lua_State *L = luaL_newstate();
+    lua_settaint(L, &test_taint);
+    vec->func(L);
+    TEST_CHECK(lua_getvaluetaint(L, 1) != NULL);
+    TEST_CHECK(!luaL_issecurevalue(L, 1));
+    lua_close(L);
+  }
+}
+
+void test_push_values_without_taint(void) {
+  for (struct LuaValueVector *vec = lua_value_vectors; vec->name; ++vec) {
+    TEST_CASE(vec->name);
+    lua_State *L = luaL_newstate();
+    vec->func(L);
+    TEST_CHECK(lua_getvaluetaint(L, 1) == NULL);
+    TEST_CHECK(luaL_issecurevalue(L, 1));
+    lua_close(L);
+  }
+}
+
+void test_push_objects_with_taint(void) {
+  for (struct LuaValueVector *vec = lua_object_vectors; vec->name; ++vec) {
+    TEST_CASE(vec->name);
+    lua_State *L = luaL_newstate();
+    lua_settaint(L, &test_taint);
+    vec->func(L);
+    TEST_CHECK(lua_getobjecttaint(L, 1) != NULL);
+    TEST_CHECK(!luaL_issecureobject(L, 1));
+    lua_close(L);
+  }
+}
+
+void test_push_objects_without_taint(void) {
+  for (struct LuaValueVector *vec = lua_object_vectors; vec->name; ++vec) {
+    TEST_CASE(vec->name);
+    lua_State *L = luaL_newstate();
+    vec->func(L);
+    TEST_CHECK(lua_getobjecttaint(L, 1) == NULL);
+    TEST_CHECK(luaL_issecureobject(L, 1));
+    lua_close(L);
+  }
+}
+
+/****************************************************************************
+ * Test Listing
+ ****************************************************************************/
+
 TEST_LIST = {
   { "initially secure on state creation", test_startup_taint },
   { "taint can be applied to state", test_tainted },
   { "taint can be forced", test_forced_taint },
   { "forced taint doesn't override existing taint", test_forced_taint_override },
+  { "pushed values inherit state taint", test_push_values_with_taint },
+  { "pushed values aren't tainted by default", test_push_values_without_taint },
+  { "pushed objects inherit state taint", test_push_objects_with_taint },
+  { "pushed objects aren't tainted by default", test_push_objects_without_taint },
   { NULL, NULL }
 };
