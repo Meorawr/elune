f5ef53ae990258eba4070d536f91786af89ac57b
Daniel Yates <me@meorawr.io>
2021-07-22 23:27:45 +0100

    Add secure constants test
    This test exercises the most annoying part of taint that has driven
    a bunch of the recent internal changes which is the propagation - or
    lack thereof - of taint on a state to constants assigned to a table.

--- tests/security.c
+++ tests/security.c
@@ -125,6 +125,69 @@ void test_secure_value_reads(void) {
 }
 
 /**
+ * Script Constant Tests
+ */
+
+void test_secure_constants(void) {
+  /**
+   * This test is a bit of an anomaly in the whole taint handling process
+   * and largely dictates why things have been implemented the way that they
+   * are.
+   *
+   * The core behavior we're attempting to replicate here is that given a
+   * securely loaded script which defines a function that assigns a constant
+   * value to a fixed table key, if the call to that function occurs
+   * insecurely then taint is _not_ applied to the field.
+   *
+   * This can be seen ingame with the following insecurely executed script:
+   *
+   *    local rect = CreateFromMixins(RectangleMixin)
+   *    rect:OnLoad()
+   *    assert(rect.left == 0, "expected 'rect.left' to be zero")
+   *    assert(not issecurevariable(rect, "left"), "expected 'rect.left' to be insecure")
+   *    rect:Reset()
+   *    assert(rect.left == 0, "expected 'rect.left' to be zero")
+   *    assert(issecurevariable(rect, "left"), "expected 'rect.left' to be secure")
+   *
+   * The Reset method in question does the following assignments:
+   *
+	 *    self.left = 0.0;
+	 *    self.right = 0.0;
+	 *    self.top = 0.0;
+	 *    self.bottom = 0.0;
+   *
+   * These end up securely overwriting any existing fields of the same name
+   * in `self` always, even if called from an insecure context.
+   *
+   * The behavior seen here implies that either taint isn't "strict"; if we
+   * implement taint through modifications to the setobj macro(s) to taint
+   * values based on the taint present in the state, these would end up
+   * insecurely assigned. Similarly this also means that taint shouldn't
+   * be propagated in the luaV_settable function, since the above snippet
+   * only generates SETTABLE opcodes.
+   */
+
+  luaT_loadfixture(LT, luac_mixin);
+  luaT_loadfixture(LT, luac_rectanglemixin);
+
+  lua_settaint(LT, &luaT_taint);
+
+  lua_getfield(LT, LUA_GLOBALSINDEX, "CreateFromMixins");
+  lua_getfield(LT, LUA_GLOBALSINDEX, "RectangleMixin");
+  luaT_checkcall(LT, 1, 1);                         /* rect = CreateFromMixins(RectangleMixin) */
+
+  lua_getfield(LT, -1, "OnLoad");
+  lua_pushvalue(LT, -2);
+  luaT_checkcall(LT, 1, 0);                         /* rect:OnLoad() */
+  TEST_CHECK(!luaL_issecurefield(LT, -1, "left"));  /* rect.left should be insecure */
+
+  lua_getfield(LT, -1, "Reset");
+  lua_pushvalue(LT, -2);
+  luaT_checkcall(LT, 1, 0);                         /* rect:Reset() */
+  TEST_CHECK(luaL_issecurefield(LT, -1, "left"));   /* rect.left should be secure */
+}
+
+/**
  * Test Listing
  */
 
@@ -139,5 +202,6 @@ TEST_LIST = {
   { "pushed objects aren't tainted by default", test_push_objects_without_taint },
   { "tainted table structures", test_tainted_table_structures },
   { "read secure values while tainted", test_secure_value_reads },
+  { "secure constants don't inherit state taint", &test_secure_constants },
   { NULL, NULL }
 };
