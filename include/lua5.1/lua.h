5d5c39ba7ea420d751b7fbaa5b23b8ce9949bbb0
Daniel Yates <me@meorawr.io>
2022-08-31 17:08:20 +0100

    Move LUA_MINSTACK to luaconf.h
    This feels more like a tunable value, and it sticks out like a sore
    thumb.

--- include/lua5.1/lua.h
+++ include/lua5.1/lua.h
@@ -85,8 +85,6 @@ typedef void * (*lua_Alloc) (void *ud, void *ptr, size_t osize, size_t nsize);
 
 
 
-/* minimum Lua stack available to a C function */
-#define LUA_MINSTACK	100
 
 
 /* predefined values in the registry */
