e871dafa1ec70980fa7eb86251316eea89930387
Daniel Yates <me@meorawr.io>
2022-12-25 16:33:35 +0000

    Make tostring use luaL_tostring

--- liblua/lbaselib.c
+++ liblua/lbaselib.c
@@ -383,27 +383,7 @@ static int luaB_xpcall (lua_State *L) {
 }
 
 static int luaB_tostring (lua_State *L) {
-    luaL_checkany(L, 1);
-    if (luaL_callmeta(L, 1, "__tostring")) { /* is there a metafield? */
-        return 1; /* use its value */
-    }
-    switch (lua_type(L, 1)) {
-        case LUA_TNUMBER:
-            lua_pushstring(L, lua_tostring(L, 1));
-            break;
-        case LUA_TSTRING:
-            lua_pushvalue(L, 1);
-            break;
-        case LUA_TBOOLEAN:
-            lua_pushstring(L, (lua_toboolean(L, 1) ? "true" : "false"));
-            break;
-        case LUA_TNIL:
-            lua_pushliteral(L, "nil");
-            break;
-        default:
-            lua_pushfstring(L, "%s: %p", luaL_typename(L, 1), lua_topointer(L, 1));
-            break;
-    }
+    luaL_tolstring(L, 1, NULL);
     return 1;
 }
 
